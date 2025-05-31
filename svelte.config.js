1851a2b3f39d1ccb7155bd33d895dd0776361720
Devin <devo1929@gmail.com>
2025-05-31 09:50:39 -0400

    add static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,17 @@
 import vercel from '@sveltejs/adapter-vercel';
 import node from '@sveltejs/adapter-node';
+import staticAdapter from '@sveltejs/adapter-static';
 
 const dockerBuild = process.env.DOCKER_BUILD
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: dockerBuild ? node() : vercel(),
+		adapter: dockerBuild ? node() : staticAdapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
 	}
 };
 
